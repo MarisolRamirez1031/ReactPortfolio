f67ccdf87adf5eb302c5a1c1e5045cc12f399556
Marisol Bennett <marisol_ramirez21@live.com>
2022-01-26 15:46:32 -0600

    added cover image

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React from "react";
+import coverImage from "../../assets/cover/peace.jpg"
 
 function About() {
-  return (
-    <section>
-      <h1 id="about">About Me, Hello.</h1>
-    </section>
-  );
+    return (
+        <section className="my-5">
+        <h1 id="about">Who am I?</h1>
+        <img src={coverImage} className="my-2" style={{ width: "100%"}} alt="cover" />
+      </section>
+    );
 }
 
-export default About;
+export default About; 
