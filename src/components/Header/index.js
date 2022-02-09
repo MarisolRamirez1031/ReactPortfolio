9a70b4010d99bc343fcc22ab4ce6f7407b349348
Marisol Bennett <marisol_ramirez21@live.com>
2022-02-08 23:11:12 -0600

    working on changing nav to header

---
+++ src/components/Header/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+
+function Header() {
+    return (
+        <header>
+        <h1><a href="./index.html">Marisol Ramirez</a></h1>
+        <nav>
+            <ul>
+                <li>
+                    <a href="#About-Me">About Me</a>
+                </li>
+                <li>
+                    <a href="#Work">Work</a>
+                </li>
+                <li>
+                    <a href="#Contact-Me">Contact Me</a>
+                </li>
+                <li>
+                    <a href="./assets/images/Resume.pdf">Resume</a>
+                </li>
+            </ul>
+        </nav>
+        </header>
+
+    )
+}
+
+export default Header; 
