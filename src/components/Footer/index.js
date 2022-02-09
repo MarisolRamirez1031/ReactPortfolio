999e9b0f770fbc99cafa377adad56965e62dfed4
Marisol Bennett <marisol_ramirez21@live.com>
2022-02-08 23:07:53 -0600

    footer contact me

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,7 +1,19 @@
 import React from "react";
 
 function Footer() {
-    
+    return (
+        <section id="Contact-Me" class="contact">
+            <div class="intro-headers">
+            <h2>Contact Me</h2>
+            </div>    
+                <ul>    
+                    <li><a href="mailto:marisolramirez1031@gmail.com">512.547.7303</a></li>
+                    <li><a href="mailto:marisolramirez1031@gmail.com">marisolramirez1031@gmail.com</a></li>
+                    <li><a href="https://github.com/MarisolRamirez1031">GitHub</a></li>
+                    <li><a href="https://www.linkedin.com">LinkedIn</a></li>
+                </ul>
+        </section>
+        )
 }
 
 export default Footer;
