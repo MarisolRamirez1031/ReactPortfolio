4afc2a1d644579fc53be48a69f809fd55db60c91
Marisol Bennett <marisol_ramirez21@live.com>
2022-02-08 22:26:18 -0600

    snapshots

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -5,11 +5,14 @@ import About from "..";
 
 afterEach(cleanup);
 
-describe('About component', () => {
-    // First Test
+describe('About component renders', () => {
     it('renders', () => {
       render(<About />);
     });
-  
-    // Second Test
+    
+    it('matches snapshot DOM node structure', () => {
+      const { asFragment } = render(<About />);
+      
+      expect(asFragment()).toMatchSnapshot();
+    });
   })
